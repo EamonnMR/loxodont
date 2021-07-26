24c124f596fd36c48a882aba73058735dc1ef7fd
EamonnMR <Eamonn.m.r@gmail.com>
2021-07-26 18:22:50 -0400

    Use Readline

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <editline/readline.h>
 
 int run(std::string line){
   std::cout << line;
@@ -10,27 +11,31 @@ int run(std::string line){
 }
 
 int runFile(char* filename){
-  std::string filenameStr(filename);
-  std::ifstream input(filenameStr);
+  std::string filenameStr { filename };
+  std::ifstream input{filenameStr};
   std::stringstream buffer;
   buffer << input.rdbuf();
-
   return run(buffer.str());
 }
 
 int runPrompt(){
+  while(true){
+    char* input_raw = readline("loxodont> ");
+    add_history(input_raw);
+    std::string input {input_raw};
+    run(input);
+  }
   return 0;
 }
-
 
 int main(int argc, char** argv ){
   // TODO: Use a real argument parsing setup
   if(argc > 2){ 
     std::cout << "Usage: loxodont [script]\n";
+    return 0;
   } else if (argc == 2){
     return runFile(argv[1]);
   } else {
-    //return runPrompt();
+    return runPrompt();
   }
-  return 0;
 }
