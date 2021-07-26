c1d1be5e36913ee814b582beb666e1d7fb578ac4
EamonnMR <Eamonn.m.r@gmail.com>
2021-07-26 17:19:53 -0400

    Read file line by line

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
+int run(std::string line){
+  std::cout << line;
+  printf("\n");
+  return 0;
+}
 int runFile(char* filename){
-  std::ifstream input(filename);
+  std::string filenameStr(filename);
+  std::ifstream input(filenameStr);
   int lastResult = 0;
   for(std::string line; getline( input, line ); ){
-    lastResult = run(line)
+    lastResult = run(line);
   }
   return lastResult;
 }
@@ -14,16 +21,15 @@ int runPrompt(){
   return 0;
 }
 
-int run
 
 int main(int argc, char** argv ){
   // TODO: Use a real argument parsing setup
-  if(argc > 1){ 
+  if(argc > 2){ 
     std::cout << "Usage: loxodont [script]\n";
-  } else if (argc == 1){
-    return runFile(argv[0]);
+  } else if (argc == 2){
+    return runFile(argv[1]);
   } else {
-    return runPrompt();
+    //return runPrompt();
   }
   return 0;
 }
