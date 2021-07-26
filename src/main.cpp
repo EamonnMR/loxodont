e5959e29bdefbb02d08f59c1f0d78b87267a3e84
EamonnMR <Eamonn.m.r@gmail.com>
2021-07-25 22:27:36 -0400

    Starting Scanning
    https://craftinginterpreters.com/scanning.html

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main(){
-  std::cout << "hello world";
+int runFile(char*){
+  return 0;
+}
+
+int runPrompt(){
+  return 0;
+}
+
+int main(int argc, char** argv ){
+  // TODO: Use a real argument parsing setup
+  if(argc > 1){ 
+    std::cout << "Usage: loxodont [script]\n";
+  } else if (argc == 1){
+    return runFile(argv[0]);
+  } else {
+    return runPrompt();
+  }
   return 0;
 }
