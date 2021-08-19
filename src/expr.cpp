04e4284ea0182981db25b31e6deeab1b53dea2fa
EamonnMR <Eamonn.m.r@gmail.com>
2021-08-19 08:45:30 +0200

    The Book uses varargs here...
    And while they appear to be possible, setting up a varadic template as a
    class member was more than I feltike doing. Can always go back and fix
    it if vectors turn out to be a bottleneck.

--- src/expr.cpp
+++ src/expr.cpp
@@ -1,12 +1,39 @@
-#import <variant>
+# include <variant>
+# include <string>
+# include <cstdint>
+# include <cstdarg>
+# include <vector>
+# include <string>
 
-#import "expr.hpp"
+# include  "expr.hpp"
 
-void ASTPrinter::operator()(Binary b){}
-void ASTPrinter::operator()(Grouping g){}
-void ASTPrinter::operator()(Literal l){}
-void ASTPrinter::operator()(Unary u){}
-
-void visit_expr(Expr e){
-  std::visit(ASTPrinter{}, e);
+std::string ASTPrinter::parenthesize(std::string name, std::vector<Expr> exprs ){
+  std::string result { "(" + name};
+  for(Expr arg : exprs){
+    result += visit(arg);
+    result += " ";
+  }
+  result += ")";
+  return result;
 }
+
+std::string ASTPrinter::operator()(Binary b){
+  return parenthesize("binary", {});
+}
+
+std::string ASTPrinter::operator()(Grouping g){
+  return parenthesize("bla", {});
+}
+
+std::string ASTPrinter::operator()(Literal l){
+  return parenthesize("literal", {});
+}
+
+std::string ASTPrinter::operator()(Unary u){
+  return parenthesize(u.op->lexeme, {*u.right});
+}
+
+std::string ASTPrinter::visit(Expr e){
+  return std::visit(e, *this);
+}
+
