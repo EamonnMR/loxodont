23b2b284b03f07ba95a256b8accc64b2db82b65f
EamonnMR <Eamonn.m.r@gmail.com>
2021-08-25 20:28:10 +0100

    Enough parsing to need memory mgmt
    This adds some basic memory mgmt to deal with the recursive data
    structures. Maybe basic is too kind a term; I'm stuffing the data onto
    the heap (with a new for every struct :sad:) and storing the pointers on
    a couple of vectors which are (pinky promise) cleaned up by the
    destructor.
    
    I'm thinking of making this more elaborite (not better, mind you) by
    using another variant so we can have just one heap object because I love
    std::visit so much apparently.
    
    Note that this is a working commit - it's missing the rest of the
    chapter so it won't compile.

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,5 +5,81 @@
 Parser::Parser(std::vector<Token> tokens){
   current = 0;
   this->tokens = tokens;
+  // TODO: Heap of variants maybe?
+  heapExpr = std::vector<Expr*>{};
+  heapToken = std::vector<Token*>{};
+}
+
+
+Expr Parser::expression(){
+  return equality();
+}
+
+Expr Parser::equality(){
+  Expr expr {comparison()};
+  while(match({BANG_EQUAL, EQUAL_EQUAL})){
+    Token op {previous()};
+    Expr right = comparison();
+    expr = Binary{alloc(expr), alloc(op), alloc(right)};
+  }
+  return expr;
+}
+
+bool Parser::match(std::vector<TokenType> types){
+  for( auto type : types){
+    if(check(type)){
+      advance();
+      return true;
+    }
+  }
+  return false;
+}
+
+bool Parser::check(TokenType type){
+  if (isAtEnd()) return false;
+  return peek().type == type;
+}
+
+Token Parser::advance() {
+  if(!isAtEnd()) current ++;
+  return previous();
+}
+
+bool Parser::isAtEnd(){
+  return peek().type == KW_EOF;
+}
+
+Token Parser::peek(){
+  return tokens[current];
+}
+
+Token Parser::previous(){
+  return tokens[current - 1];
+}
+
+/* Manual memory mgmt
+ * Note that these are deleted in the destructor
+ */
+// Todo: Malloc more sparingly
+
+Expr* Parser::alloc(Expr expr){
+  Expr* xpr = new Expr{expr};
+  heapExpr.push_back(xpr);
+  return xpr;
+}
+
+Token* Parser::alloc(Token token){
+  Token* tkn = new Token{token};
+  heapToken.push_back(tkn);
+  return tkn;
+}
+
+Parser::~Parser(){
+  for(auto expr : heapExpr){
+    delete expr;
+  }
+  for(auto token : heapToken){
+    delete token;
+  }
 }
 
--- src/parser.hpp
+++ src/parser.hpp
@@ -1,11 +1,32 @@
 # pragma once
 
 # include "token.hpp"
+# include "expr.hpp"
 # include <vector>
 
 struct Parser {
-  std::vector<Token> tokens;
+  std::vector<Token> tokens;  // Input
+  // Parser-allocated memory
+  std::vector<Expr*> heapExpr;
+  std::vector<Token*> heapToken;
   size_t current;
-  Parser(std::vector<Token>);
-};
 
+  Parser(std::vector<Token>);
+  ~Parser();
+  // Grammar Rules
+  Expr expression();
+  Expr equality();
+  Expr comparison();
+
+  // Utility
+  bool match(std::vector<TokenType>);
+  bool check(TokenType);
+  Token advance();
+  bool isAtEnd();
+  Token peek();
+  Token previous();
+
+  // Utility (Not in the book)
+  Expr * alloc(Expr);
+  Token * alloc(Token);
+};
